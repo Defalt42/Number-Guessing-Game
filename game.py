bc6ba2ef23808a67e5894728f0b791ec7f431c8b
Bryan Gilbert <gilbertb4375@gmail.com>
2018-12-21 14:01:19 -0600

    Add guessing logic

--- game.py
+++ game.py
@@ -3,5 +3,19 @@ import random
 # Generate random number between 1 and 100
 rand_num = random.randint(1, 100)
 
+while True:
+	guess = int(input("Guess a whole number between 1 and 100: "))
+
+	if guess < 1 or guess > 100:
+		print(guess + " is not between 1 and 100. Try again.")
+		continue
+	else:
+		if guess > rand_num:
+			print("{} is too high. Try again.".format(guess))
+		elif guess < rand_num:
+			print("{} is too low. Try again.".format(guess))
+		else:
+			print("{} is correct!".format(guess))
+			break
 
 	
