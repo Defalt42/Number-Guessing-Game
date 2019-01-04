0d56be993d9b5bd92f435e9ff06aa47f973c1d7f
Bryan Gilbert <gilbertb4375@gmail.com>
2019-01-04 13:54:49 -0600

    Add comments

--- game.py
+++ game.py
@@ -7,15 +7,17 @@ num_guesses = 0
 
 while num_guesses < MAX_GUESSES:
 	print("Guesses left: {}".format(MAX_GUESSES - num_guesses))
+	# Set guess to user input if user inputs a whole number
 	try:
 		guess = int(input("Guess a whole number between 1 and 100: "))
 	except ValueError:
 		print("That is not a whole number. Try again.")
 		continue
-
+	# Verify that the user's guess is within the range 1 to 100 inclusive
 	if guess < 1 or guess > 100:
 		print(str(guess) + " is not between 1 and 100. Try again.")
 		continue
+	# User input is a whole number; provide feedback as to how close the user's guess is to the random number
 	else:
 		num_guesses += 1
 		if guess > rand_num:
@@ -25,6 +27,7 @@ while num_guesses < MAX_GUESSES:
 		else:
 			print("{} is correct!".format(guess))
 			break
+# Checks if the user correctly guessed the number or if they ran out of guesses
 if num_guesses >= 10 and guess != rand_num:
 	print("Out of guesses. Play again sometime.")
 else:
